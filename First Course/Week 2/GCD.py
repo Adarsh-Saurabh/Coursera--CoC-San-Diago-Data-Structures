ad764e47b1c9d42db3656c9984ef68665c983b91
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-07-27 22:56:30 +0530

    Update GCD.py

--- First Course/Week 2/GCD.py
+++ First Course/Week 2/GCD.py
@@ -1,21 +1,75 @@
+import sys
+
 def gcd(a,b):
-    if a > b:
-        arr = [a,b]
-    elif a == 0:
-        return b
-    elif b == 0:
-        return a
+  
+    if a == b or b == a:
+        return a 
+    elif a == 1 :
+        return 1
+    elif   b ==1:
+        return 1
+    elif a > b:
+        if b == 0:
+            return a
+        else:
+            arr = [a,b] 
     else :
-        arr = [b,a]
-    for i in range(2 , min(a,b)):
+        if a == 0:
+            return b
+        else:    
+            arr = [b,a]
+    for i in range(2 , max(a,b)):
         c = arr[i - 2] % arr[i - 1]
         arr.append(c)
         if c == 0 :
             break
     return arr[i-1]
              
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(gcd(a,b))
+if __name__ == "__main__":
+    input = sys.stdin.read()
+    # a , b = 18,35
+    a, b = map(int, input.split())
+    print(gcd(a, b))
 
+
+
+# def gcd(a, b): 
+
+  
+
+#     # Everything divides 0 
+
+#     if (a == 0): 
+
+#         return b 
+
+#     if (b == 0): 
+
+#         return a 
+
+  
+
+#     # base case 
+
+#     if (a == b): 
+
+#         return a 
+
+  
+
+#     # a is greater 
+
+#     if (a > b): 
+
+#         return gcd(a-b, b) 
+
+#     return gcd(a, b-a) 
+
+  
+# # Driver program to test above function 
+
+# a = int(input())
+
+# b = int(input())
+
+# print(gcd(a,b)) 
