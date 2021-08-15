e8d466457bd27bd1a15138d785388a79dcb129cb
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-08-16 00:46:29 +0530

    Update Practice.py

--- First Course/Week 3/Car fueling/Practice.py
+++ First Course/Week 3/Car fueling/Practice.py
@@ -35,57 +35,59 @@ import sys
 #                     return num_refills
 #             return num_refills
 
-def compute_min_refills(distance , tank , stops):
-    length , refill = 0,0
-    stops.append(distance)
-    z = 0
-    stops.insert(0,z)
-    stops.sort()
+# def compute_min_refills(distance , tank , stops):
+#     length , refill = 0,0
+#     stops.append(distance)
+#     z = 0
+#     stops.insert(0,z)
+#     stops.sort()
 
-    # print(stops)
-    for i in range(len(stops) - 1):
-        if stops[i+1] - stops[i] > tank:
-            # print(23)
-            return -1
-    if tank >= distance:
-        return 0
-    i = 0
-    # for i in range(len(stops)):
-    while length < stops[-1]:
-        # print(stops)
-        random = []
-        refill += 1
-        # print(len(stops))
-        # print(i)
-        # for j in range(len(stops)):
-        #     if distance <= stops[j]:
-        #         refill = refill -1
-        #         break
-        while length + tank >= stops[i]:
-            random.append(stops[i])
-            # print(i)
-            i += 1
-            if i == len(stops) :
-                break
-        # print(i)
-        length += tank
-        x = max(random)
-        # print(length)
-        if distance <= stops[i-1]:
-            refill -= 1
-        if x == stops[-2]:
-            return refill
-    return refill     
+#     # print(stops)
+#     for i in range(len(stops) - 1):
+#         if stops[i+1] - stops[i] > tank:
+#             # print(23)
+#             return -1
+#     if tank >= distance:
+#         return 0
+#     i = 0
+#     # for i in range(len(stops)):
+#     while length < stops[-1]:
+#         # print(stops)
+#         random = []
+#         refill += 1
+#         # print(len(stops))
+#         # print(i)
+#         # for j in range(len(stops)):
+#         #     if distance <= stops[j]:
+#         #         refill = refill -1
+#         #         break
+#         while length + tank >= stops[i]:
+#             random.append(stops[i])
+#             # print(i)
+#             i += 1
+#             if i == len(stops) :
+#                 break
+#         # print(i)
+#         length += tank
+#         x = max(random)
+#         # print(length)
+#         if distance <= stops[i-1]:
+#             refill -= 1
+#         if x == stops[-2]:
+#             return refill
+#     return refill     
                 
 
 
 
 
+
+
 if __name__ == '__main__':
-    distance, tank, _, *stops = map(int, sys.stdin.read().split())
+    # distance, tank, _, *stops = map(int, sys.stdin.read().split())
     # print(compute_min_refills(d, m, stops))
 
-    # distance = 240
-    # tank = 50
-    # stops = [50,100 , 190]
+    distance = 240
+    tank = 50
+    stops = [50,100 , 190]
     print(compute_min_refills(distance, tank, stops))
