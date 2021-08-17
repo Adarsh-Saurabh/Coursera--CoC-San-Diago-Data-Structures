5e4a2bb398cff0c9514f294295d09b23a95d508c
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-08-17 23:44:10 +0530

    change

--- First Course/Week 3/Knapsack problem/Stress Test Knapsack problem.py
+++ First Course/Week 3/Knapsack problem/Stress Test Knapsack problem.py
@@ -1,5 +1,5 @@
-
 import sys
+
 
 def get_optimal_value(capacity, weights, values):
     value = 0
@@ -30,43 +30,27 @@ def get_optimal_value(capacity, weights, values):
             weights.remove(weights[i])
             values.remove(values[i])
 
-def get(capacity , weights , values):  
+
+def get(capacity , weights , values):
     opt_value = 0
     items = list(zip(values, weights))
-    print(items)
     items.sort(key=lambda item: item[0] / item[1], reverse=True)
     values = [item[0] for item in items]
     weights = [item[1] for item in items]
     for value, weight in zip(values, weights):
         if capacity == 0:
-            return round(opt_value, 4)
+            return round(opt_value , 4)
         min_weight = min(weight, capacity)
         opt_value += min_weight * (value / weight)
         weight -= min_weight
         capacity -= min_weight
-
-    return round(opt_value, 4)
+    return round(opt_value , 4)
 
 
 if __name__ == "__main__":
-    # # weights = [20,50,30]
-    # # values = [60,100,120]
-    # # weights = [20,50,30,10,50,40]
-    # # values = [60,100,120,110,160,50]
-    # weights = [20]
-    # values = [100]
-    # capacity = 5
-    # print(get_optimal_value(capacity, weights, values))
-    
-    # data = list(map(int, sys.stdin.read().split()))
-    # n, capacity = data[0:2]
-    # values = data[2:(2 * n + 2):2]
-    # weights = data[3:(2 * n + 2):2]
-    # opt_value = get_optimal_value(capacity, weights, values)
-    # print("{:.10f}".format(opt_value))
     import random
     while True:
-        n = random.randint(1,10)
+        n = random.randint(1,1000)
         wei = []
         val = []
         for i in range(n):
@@ -74,10 +58,20 @@ if __name__ == "__main__":
             val.append(random.randint(1,100000))
         weights = wei
         values = val
-        capacity = random.randint(10 , 1000)
-        if get(capacity , weights , values) != get_optimal_value(capacity, weights, values) :
-            print( get(capacity, weights, values) ,  get_optimal_value(capacity, weights, values)  )
+        capacity = random.randint(10 , 100000)
+        if get(capacity , weights , values) == get_optimal_value(capacity , weights , values):
+            # print("OK")
+            continue
+        else:
+            print( get(capacity, weights, values) , " " ,  get_optimal_value(capacity, weights, values))
             print(weights )
             print(values)
             print(capacity)
-            break
+            print("Wrong Answer")
+          
+
+        
+            
+
+
+    
